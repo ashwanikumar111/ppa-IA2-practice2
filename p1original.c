a681ed962d0f3b776d62d81fc858632183fe1fd5
ashwanikumar111 <96907813+ashwanikumar111@users.noreply.github.com>
2022-02-07 18:29:07 +0000

    coding

--- p1original.c
+++ p1original.c
@@ -1,2 +1,11 @@
+#include<stdio.h>
+int main() {
+  int a, b, area;
+  printf("the base and height of the triangle are: \n ");
+  scanf("%d %d", &a, &b);
+  area =(a*b)/2;
+  printf("the area of the triangle is %d : ", area);
 
+  return area;
 }
+
