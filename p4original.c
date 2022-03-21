115dc6c7b34daa3aaaf415a8ae169a0614b95a7f
ashwanikumar111 <96907813+ashwanikumar111@users.noreply.github.com>
2022-03-21 06:45:13 +0000

    coding

---
+++ p4original.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int input_array_size()
+{
+int n;
+printf("enter the number\n");
+scanf("%d", &n);
+return n;
+}
+void input_array(int n,int a[n])
+{
+int i;
+printf("enter %d numbers :\n", n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+}
+int sum_composite(int n, int a[n])
+{
+int count = 0, i;
+  for(i=0;i<n;i++)
+    {
+      if(a[i]==2)
+    {
+continue;
+}
+else if(a[i]%2==0)
+{
+count=count+a[i];
+}
+} 
+return count;
+}
+void  output(int sum)
+{
+printf("sum of all composite number is %d", sum);
+}
+int main()
+{
+int n,result;
+n=input_array_size();
+int a[n];
+input_array(n,a);
+result=sum_composite(n,a);
+output(result);
+return 0;
+}
+  
