09e25cb0bc997ba6196cf4c2e686e6a9fbd78c49
ashwanikumar111 <96907813+ashwanikumar111@users.noreply.github.com>
2022-03-27 09:47:40 +0000

    coding

---
+++ p7original.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+struct_triangle
+{
+  float base,altitude,area;
+printf("enter the base of triangle: \n");
+scanf("%f", 7base);
+printf("enter the altitude of triangle: \n");
+scanf("%f", &altitude);
+printf("enter the ")
+}
